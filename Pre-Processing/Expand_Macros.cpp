4a0b82104401dbb5032b5cf2eadaffbccb90bf67
Sriyukthika26 <23je0967@iitism.ac.in>
2025-07-26 11:31:34 +0530

    Expand_Macros

---
+++ Pre-Processing/Expand_Macros.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string line;
+	
+	ifstream infile("Inputs/Input_Macros.cpp");
+	ofstream outfile("Outputs/Expanded_Macros.cpp");
+	
+	unordered_map <string , string> macros;
+	vector<string> codeLines;
+	
+	while(getline(infile, line))
+	{	
+		int i = 0;
+		while(i<line.length() && isspace(line[i])) i++;
+		if(i+1 < line.length() && line[i] == '#' && line[i+1] == 'd')
+		{
+			istringstream iss(line.substr(i));
+			string directive;
+			iss>> directive;
+			
+			if(directive == "#define")
+			{
+				string key, value;
+				iss>>key;
+				getline(iss, value);
+				value = regex_replace(value, regex("^ +| +$"), "");
+				if(!key.empty() && !value.empty())
+				{
+					macros[key] = value;
+				}
+			}
+			continue;
+		}
+		codeLines.push_back(line);
+	}
+	
+	for(string &codeLine : codeLines)
+	{
+		for(const auto &macro : macros)
+		{
+			codeLine = regex_replace(codeLine, regex("\\b"+ macro.first + "\\b"), macro.second);
+		}
+		outfile<<codeLine<<endl;
+	}
+	
+	infile.close();
+	outfile.close();
+	return 0;
+}
